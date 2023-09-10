5bb5c39f90a676b4832625230785b29e91282ca1
Anastassiya <av204@student.london.ac.uk>
2023-09-10 23:27:28 +0800

    First Project Finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,45 @@ import { useState } from 'react'
 import { gpt_3_5_Request } from './customFetch'
 
 export default function App() {
+	const [chats, setChats] = useState([])
 	const [query, setQuery] = useState('')
 	const [chatResponse, setChatResponse] = useState(null)
+	const [isLoading, setIsLoading] = useState(false)
 
 	const handleSubmit = async e => {
 		e.preventDefault()
+		setIsLoading(true)
 		const response = await gpt_3_5_Request(query)
-		setQuery('')
+		setIsLoading(false)
 		setChatResponse(response)
+		setChats(prev => [...prev, { query, response }])
+		setQuery('')
+	}
+
+	const handleHistory = id => {
+		setChatResponse(chats[id].response)
 	}
 
 	return (
 		<div className='topLevelContainer'>
 			<aside className='side-bar'>
-				<button className='newChatButton'>New Chat</button>
-				<div className='history'></div>
+				<button className='newChatButton' onClick={() => setChatResponse('')}>
+					New Chat
+				</button>
+				<div className='history'>
+					{chats.map((item, index) => (
+						<p key={index} onClick={() => handleHistory(index)}>
+							{item.query}
+						</p>
+					))}
+				</div>
 				<nav>
 					<p>Made For S3 Company</p>
 				</nav>
 			</aside>
 			<main className='main'>
 				<h1>MyGPT</h1>
-				<p id='output'>{chatResponse}</p>
+				<p id='output'>{isLoading ? 'LOADING...' : chatResponse}</p>
 				<form className='input-container' onSubmit={handleSubmit}>
 					<input
 						type='text'
@@ -36,9 +53,7 @@ export default function App() {
 						{'\u27a2'}
 					</button>
 				</form>
-				<p className='info'>
-					3 requests per minute. 200 request per day. GPT-3.5 Turbo.
-				</p>
+				<p className='info'>GPT-3.5 Turbo.</p>
 			</main>
 		</div>
 	)
